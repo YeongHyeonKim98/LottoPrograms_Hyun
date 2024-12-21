bb97eeb04a0f7ea2beb92e65363c7ab552e43346
Hyun <ikimyh22@naver.com>
2024-12-21 12:44:02 +0900

    Initial Commit

---
+++ LottoPrograms_Hyun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottoPrograms_Hyun
+//
+//  Created by 김영현 on 12/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LottoPrograms_HyunTests/LottoPrograms_HyunTests.swift
@@ -0,0 +1,17 @@
+//
+//  LottoPrograms_HyunTests.swift
+//  LottoPrograms_HyunTests
+//
+//  Created by 김영현 on 12/21/24.
+//
+
+import Testing
+@testable import LottoPrograms_Hyun
+
+struct LottoPrograms_HyunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
